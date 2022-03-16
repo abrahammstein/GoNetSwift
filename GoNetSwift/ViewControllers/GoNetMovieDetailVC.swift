f6899024ce0eac8c10aa63db76c207d509f67c6b
jabrahamlopez <Jose.Arciniega@dominos.com>
2022-03-16 18:40:43 -0400

    Added ScrollView
    Added ScrollView to Detail Screen

--- GoNetSwift/ViewControllers/GoNetMovieDetailVC.swift
+++ GoNetSwift/ViewControllers/GoNetMovieDetailVC.swift
@@ -13,6 +13,8 @@ class GoNetMovieDetailVC: UIViewController {
     let movieImageView = GoNetImageView(frame: .zero)
     let summaryLabel   = GoNetBodyLabel(textAlignment: .center)
     let imdbLabel      = GoNetBodyLabel(textAlignment: .center)
+    let scrollView     = UIScrollView()
+    let scrollViewContainer = UIStackView()
     var navButton: UIBarButtonItem!
     
     init(movie: MovieElement) {
@@ -104,30 +106,49 @@ class GoNetMovieDetailVC: UIViewController {
             imdbLabel.addGestureRecognizer(tapURLGesture)
         }
         
+        scrollViewContainer.axis = .vertical
+        scrollViewContainer.spacing = 10
+        scrollViewContainer.translatesAutoresizingMaskIntoConstraints = false
         
-        self.view.addSubview(movieImageView)
-        self.view.addSubview(summaryLabel)
-        self.view.addSubview(imdbLabel)
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(scrollView)
+        self.scrollView.addSubview(scrollViewContainer)
+        self.scrollViewContainer.addArrangedSubview(movieImageView)
+        self.scrollViewContainer.addArrangedSubview(summaryLabel)
+        self.scrollViewContainer.addArrangedSubview(imdbLabel)
         let padding: CGFloat = 8
         let leadingTrailingPadding: CGFloat = 20
         let height: CGFloat = 200
         
+        scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        scrollView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
+        scrollViewContainer.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor).isActive = true
+        scrollViewContainer.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor).isActive = true
+        scrollViewContainer.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
+        scrollViewContainer.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
+        
+        scrollViewContainer.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
+
+        
         NSLayoutConstraint.activate([
-            movieImageView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 150),
-            movieImageView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: leadingTrailingPadding),
-            movieImageView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -leadingTrailingPadding),
+            movieImageView.topAnchor.constraint(equalTo: self.scrollView.topAnchor, constant: 20),
+            movieImageView.leadingAnchor.constraint(equalTo: self.scrollView.leadingAnchor, constant: leadingTrailingPadding),
+            movieImageView.trailingAnchor.constraint(equalTo: self.scrollView.trailingAnchor, constant: -leadingTrailingPadding),
             movieImageView.heightAnchor.constraint(equalToConstant: height),
-            
+
             summaryLabel.topAnchor.constraint(equalTo: movieImageView.bottomAnchor, constant: 8),
-            summaryLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: padding),
-            summaryLabel.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -padding),
+            summaryLabel.leadingAnchor.constraint(equalTo: self.scrollView.leadingAnchor, constant: padding),
+            summaryLabel.trailingAnchor.constraint(equalTo: self.scrollView.trailingAnchor, constant: -padding),
             summaryLabel.heightAnchor.constraint(equalToConstant: 100),
-            
+
             imdbLabel.topAnchor.constraint(equalTo: summaryLabel.bottomAnchor, constant: 8),
-            imdbLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: padding),
-            imdbLabel.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -padding),
+            imdbLabel.leadingAnchor.constraint(equalTo: self.scrollView.leadingAnchor, constant: padding),
+            imdbLabel.trailingAnchor.constraint(equalTo: self.scrollView.trailingAnchor, constant: -padding),
             imdbLabel.heightAnchor.constraint(equalToConstant: 100),
-            imdbLabel.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant:-20)
+            imdbLabel.bottomAnchor.constraint(equalTo: self.scrollView.bottomAnchor, constant:-20)
         ])
     }
 
